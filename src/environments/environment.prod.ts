c3143ece84c46197ee8108c1276283e45a3f5635
Laura Romero <113153668+ldromeromo@users.noreply.github.com>
2024-11-10 10:32:31 -0500

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  
+  apiUrl: 'http://localhost:8000/bar/service/v1',
+  url_api: 'http://localhost:8000/bar/service/v1',
+  //permisos
+  can_view_module_inyector_automatico: 113
+};
